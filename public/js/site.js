3df337f4d465c0f58a9eed4ef38636019bdf2e85
Oliver Wise <owise1@gmail.com>
2015-09-17 09:38:43 -0700

    local js files, remove many morse audios, colophon

--- public/js/site.js
+++ public/js/site.js
@@ -11778,7 +11778,7 @@ $(function () {
     setTimeout(doSlider, 1000);
   });
 });
-}).call(this,require("1YiZ5S"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_2f83103f.js","/")
+}).call(this,require("1YiZ5S"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_6ab57e37.js","/")
 },{"./soundmanager2.js":12,"1YiZ5S":4,"buffer":1,"imagesLoaded":5,"morsecode":8,"q":9,"ramda":10}],12:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 /** @license
